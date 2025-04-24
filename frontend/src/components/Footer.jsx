14cb20a35d85b0355d59105732687221dbe5daa3
Suraj sahu <84985473+Surajsahu01@users.noreply.github.com>
2025-04-24 19:51:51 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <Box mt={5} py={3} textAlign="center" bgcolor="#f5f5f5">
       <Typography variant="body2" color="textSecondary">
-        © {new Date().getFullYear()} Seed Protection System — Built with ❤️ using MERN & IoT
+        © {new Date().getFullYear()} Seed Protection System — Built with MERN STACK & IoT
       </Typography>
     </Box>
   )
