bb382c6606960f254767729394bd57647ac35bc9
Suraj sahu <84985473+Surajsahu01@users.noreply.github.com>
2025-04-24 19:08:21 +0530

    Update util.js

--- frontend/src/Utils/util.js
+++ frontend/src/Utils/util.js
@@ -3,4 +3,4 @@
 export const BLYNK_AUTH_TOKEN = "wISauJLuCxAWeZU1sXQ8LH2dUnPV1zK7";
 export const BLYNK_URL = `https://blynk.cloud/external/api/update`;
 
-export const API_BASE_URL="http://localhost:4000";
+export const API_BASE_URL="https://seed-protection-backend.onrender.com";
